64993966c191762f5f42ef244722ef94759c3831
Daria Kolesnik <daria.gris@bk.ru>
2026-04-07 17:00:50 +0300

    Сохраняю работу перед обновлением

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,9 +23,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
     'users.apps.UsersConfig',
-    'api',
-    'reviews',
 ]
 
 MIDDLEWARE = [
