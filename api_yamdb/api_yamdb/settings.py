d65804a6abe5e8de8027da93b0993fb91cc88cc8
Daria Kolesnik <daria.gris@bk.ru>
2026-04-07 17:57:24 +0300

    Промежуточное сохранение работы над пользователями

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 
 
@@ -27,6 +28,7 @@ INSTALLED_APPS = [
     'reviews.apps.ReviewsConfig',
     'users.apps.UsersConfig',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +109,17 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -4,19 +4,29 @@ from django.db import models
 
 
 class YamdbUser(AbstractUser):
+    """Модель пользователя."""
     username = models.CharField(
         max_length=150,
         unique=True,
         validators=[RegexValidator(
             regex=r'^[\w.@+-]+\Z',
-            message='Некорректное имя пользователя.',
+            message=(
+                'Поле обязательно для заполнения. '
+                'Может содержать только буквы, цыфры и символы @/./+/-/_.'
+            ),
             code='invalid_username'
         )],
+        verbose_name='Имя пользователя'
     )
     email = models.EmailField(
         unique=True,
         max_length=254,
+        verbose_name='Почта'
     )
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+    bio = models.TextField(verbose_name='Биография пользователя')
+    role = models.CharField(max_length=100, default='user')
 
     class Meta:
         verbose_name = 'Пользователь'
