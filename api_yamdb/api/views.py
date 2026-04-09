4bca43b1194a66bf069eaf86cb8de5e610db5d9f
petroocho <fox111085@gmail.com>
2026-04-09 16:15:07 +0500

    feat: add Review, Comment models, serializers, viewsets, rating for Title

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,15 +1,19 @@
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, viewsets
 
-from reviews.models import Category, Genre, Title
+from reviews.models import Category, Genre, Review, Title
 from api.filters import TitleFilter
 from api.serializers import (
     CategorySerializer,
+    CommentSerializer,
     GenreSerializer,
-    TitleWriteSerializer,
-    TitleReadSerializer
+    # TitleWriteSerializer,
+    # TitleReadSerializer,
+    TitleSerializer,
+    ReviewSerializer,
 )
-from api.permissions import IsAdminOrReadOnly
+from api.permissions import IsAdminOrReadOnly, IsAuthorModeratorAdmin
 
 
 class CategoryViewSet(
@@ -47,13 +51,54 @@ class GenreViewSet(
 class TitleViewSet(viewsets.ModelViewSet):
     """Вьюсет для произведений."""
 
+    serializer_class = TitleSerializer
     queryset = Title.objects.all()
     filterset_class = TitleFilter
     filter_backends = (DjangoFilterBackend,)
     permission_classes = (IsAdminOrReadOnly, )
     http_method_names = ('get', 'post', 'patch', 'delete', 'head', 'options')
 
-    def get_serializer_class(self):
-        if self.action in ('list', 'retrieve'):
-            return TitleReadSerializer
-        return TitleWriteSerializer
+    # def get_serializer_class(self):
+    #     if self.action in ('list', 'retrieve'):
+    #         return TitleReadSerializer
+    #     return TitleWriteSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """Вьюсет для отзывов."""
+
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthorModeratorAdmin,)
+    http_method_names = ('get', 'post', 'patch', 'delete', 'head', 'options')
+
+    def get_title(self):
+        """Возвращает произведение по title_id из URL."""
+        return get_object_or_404(Title, id=self.kwargs.get('title_id'))
+
+    def get_queryset(self):
+        """Возвращает все отзывы к конкретному произведению."""
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        """При создании отзыва сохраняет автора и произведение."""
+        serializer.save(author=self.request.user, title=self.get_title())
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Вьюсет для комментариев."""
+
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorModeratorAdmin,)
+    http_method_names = ('get', 'post', 'patch', 'delete', 'head', 'options')
+
+    def get_review(self):
+        """Возвращает отзыв по review_id из URL."""
+        return get_object_or_404(Review, id=self.kwargs.get('review_id'))
+
+    def get_queryset(self):
+        """Возвращает все комментарии к конкретному отзыву."""
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        """Сохраняет комментарий с автором и привязкой к отзыву."""
+        serializer.save(author=self.request.user, review=self.get_review())
